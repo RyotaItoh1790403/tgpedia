316e15d44d257e831183f816624e5e86efe39cfe
Ryota Itoh <ry-itou@tg-inet.co.jp>
2019-10-02 02:14:07 +0900

    Initial Commit

---
+++ tgpedia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tgpedia
+//
+//  Created by ryota itoh on 2019/10/02.
+//  Copyright © 2019 ryota itoh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
